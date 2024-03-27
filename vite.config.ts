918061ee3a7450e092f95817d1ca545fe20dd505
8-88 <84121208+8-88@users.noreply.github.com>
2024-03-28 00:13:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path";
 
 export default defineConfig({
-    base: './react-h5-mobile-template',
+    base: './react-h5-mobile-template/',
     plugins: [react()],
     server: {
         port: 3000,
